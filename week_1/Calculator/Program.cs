d917ba3ec6e883f2026044c09254d90179e660ba
casey peng <casey.peng@revature.net>
2021-08-18 13:05:21 -0400

    Make Csharp Calculator

---
+++ week_1/Calculator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          var input1 = int.Parse(Console.ReadLine());
+          var input2 = int.Parse(Console.ReadLine());
+
+          var result1 = Add(input1,input2);
+          var result2 = Sbutract(input1,input2);
+
+            Console.WriteLine(result1,result2);
+         
+        }
+
+        static void Add()
+        {
+          //input stuff
+          //casting taking the whole thing and covert it to a new type 
+
+      
+          //comput stuff
+
+          var compute = (int)input1 + (int)input2;
+          
+          
+        }
+        static void Sbutract()
+        {
+          //input stuff
+          //casting taking the whole thing and covert it to a new type 
+
+      
+          //comput stuff
+
+          var compute = (int)input1 - (int)input2;
+          //output
+        
+        }
+        
+        
+    }
+}
